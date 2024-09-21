b7f643192972bd16884f24f1e111fad339611062
Набиуллина Амина Радиковна <152320@mck-ktits.ru>
2024-09-21 09:11:56 +0300

    вывод с помощью alert

---
+++ sm1/main.js
@@ -0,0 +1,16 @@
+let name = prompt("Как тебя зовут?");
+let age = prompt("Сколько лет?");
+if (age < 20) {
+    alert("Тебе меньше 20");
+} 
+else {
+    for (let i = 0; i < 5; i++) {
+        alert("Привет!");
+    }
+}
+let JS = confirm("Хочешь учить js?");
+if (JS) {
+    alert("Супер!");
+}
+
+
