648442e352c3195932cba90c8635c7afc79c64b8
naamll <aminanabiullina85@gmail.com>
2024-09-28 10:04:01 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+// Тот самый массив
+const developers = [
+    { name: 'John', surname: 'Doe', age: 20, talk: () => { console.log('Hello world!') } },
+    { name: 'Василий', surname: 'Иванов', age: 21 },
+    { name: 'Екатерина', surname: 'Демидова', age: 21 },
+    { name: 'Пётр', surname: 'Шереметьев', age: 19 },
+];
+
+const newDeveloper = {
+    name: prompt('HR: Как Вас зовут?'),
+    surname: prompt('HR: Какая у Вас фамилия?'),
+    age: prompt('HR: Сколько Вам лет?'),
+};
+
+let exp;
+
+
+while (!Number.isInteger(parseInt(exp))) {
+    exp = prompt('HR: Сколько лет опыта в it?');
+}
+
+if (exp < 3) {
+    alert('HR: Прости, но у тебя слишком мало опыта :(');
+} else {
+    alert("Lead: Добро пожаловать в команду!");
+    alert("Lead: Поприветствуйте нового коллегу!");
+
+
+
+    developers.map(dev => {
+        alert('${dev.name}: Привет!');
+    });
+
+
+    developers.push(newDeveloper);
+}
+
+
+
+console.log(developers);  
